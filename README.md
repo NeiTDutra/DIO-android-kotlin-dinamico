83b800ca4c6e358eb82a4d24a4cc7b36be7f7e31
unknown <nei.thomass@gmail.com>
2021-04-17 18:44:12 -0300

    enviando os arquivos do primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Navegação, aparência e estilo de aplicativos android
+
+Conceitos de navegação, aparência e estilo de aplicativos Android.
+Bootcamp Everis Kotlin Developer da [Digital Innovation One Inc.](https://web.digitalinnovation.one).
---
+++ app/src/main/java/com/example/dio_kotlin_nav_apare_estilo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dio_kotlin_nav_apare_estilo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
